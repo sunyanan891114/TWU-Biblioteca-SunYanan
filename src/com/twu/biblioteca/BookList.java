4a4016e47e006f8b6d9a0cd91df580ed5635eb95
sun.yanan <sun.yanan@rea-group.com>
2016-04-11 21:27:04 +0800

    Write tests for BookList and create BookList to show books

---
+++ src/com/twu/biblioteca/BookList.java
@@ -0,0 +1,10 @@
+package com.twu.biblioteca;
+
+import java.util.ArrayList;
+
+public class BookList {
+
+  public ArrayList<Book> showList() {
+    return null;
+  }
+}
---
+++ test/com/twu/biblioteca/BookListTest.java
@@ -0,0 +1,23 @@
+package com.twu.biblioteca;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
+
+public class BookListTest {
+  public BookList bookList;
+  public ArrayList<Book> booklist;
+  @Before
+  public void initialize() {
+    bookList = new BookList();
+    booklist = new ArrayList<Book>();
+  }
+  @Test
+  public void showListTest()
+  {
+    assertEquals(bookList.showList(), booklist);
+  }
+}
+
