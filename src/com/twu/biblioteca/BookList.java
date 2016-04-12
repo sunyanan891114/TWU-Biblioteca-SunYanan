29e50c07adf3ffbed8a0db8558197a26eda605fe
sun.yanan <sun.yanan@rea-group.com>
2016-04-12 16:08:33 +0800

    Add libarary a book list to class BookList.

--- src/com/twu/biblioteca/BookList.java
+++ src/com/twu/biblioteca/BookList.java
@@ -3,6 +3,15 @@ package com.twu.biblioteca;
 import java.util.ArrayList;
 
 public class BookList {
+  private ArrayList<Book> libarary;
+
+  public ArrayList<Book> getLibarary() {
+    return libarary;
+  }
+
+  public void setLibarary(ArrayList<Book> libarary) {
+    this.libarary = libarary;
+  }
 
   public ArrayList<Book> showList() {
     return null;
---
+++ test/com/twu/biblioteca/MainMenuTest.java
@@ -0,0 +1,25 @@
+package com.twu.biblioteca;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainMenuTest {
+  public Book book;
+  @Before
+  public void initialize() {
+    book = new Book();
+  }
+  @Test
+  public void testGetAuthor() {
+    assertEquals(book.getAuthor(), "syn");
+  }
+
+  @Test
+  public void testGetYearPublished() {
+    assertEquals(book.getYearPublished(), "1989");
+  }
+}
+
