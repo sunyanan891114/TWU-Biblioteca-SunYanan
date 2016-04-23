0ed044415d9c85c1a8bd0460dd74837ed5499a9a
sun.yanan <sun.yanan@rea-group.com>
2016-04-23 13:27:00 +0800

    tests for the show list option

--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -1,22 +1,38 @@
 package com.twu.biblioteca;
 
-
+import org.junit.After;
 import org.junit.Before;
-
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 
 public class LibraryTest {
-  public static Library bookList;
+  public static Library library;
   public ArrayList<Book> booklist;
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
   @Before
   public void initialize() {
-    bookList = Library.getBookListInstance();
+    library = Library.getBookListInstance();
     booklist = new ArrayList<Book>();
+    booklist.add(new Book("syn","1989",1));
+    library.setBooklist(booklist);
   }
-//  @Test
-//  public void testShowList() {
-////    assertEquals(bookList.showList(), booklist);
-//  }
+  @Before
+  public void setUpStreams() {
+    System.setOut(new PrintStream(outContent));
+  }
+
+  @After
+  public void cleanUpStreams() {
+    System.setOut(null);
+  }
+  @Test
+  public void testShowList() {
+    library.showList();
+    String test = "   AUTHOR |  PUBLISHED YEAR \n" + "      syn |            1989 \n";
+    assertEquals(outContent.toString(), test);
+  }
 }
 
